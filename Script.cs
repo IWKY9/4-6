f93c5b5663e38e8e612a869d763f042c1c9bd966
IWKY9 <163404217+IWKY9@users.noreply.github.com>
2024-03-14 15:34:25 +0500

    Create Script.cs

---
+++ Script.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Script : MonoBehaviour
+{
+    public float speed = 1f;
+    public float jumpForce = 5f;
+    bool Jump = false;
+    Rigidbody2D rb;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float movement = Input.GetAxisRaw("Horizontal");
+        transform.position += new Vector3(movement, 0, 0) * speed * Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.Space) && Mathf.Abs(rb.velocity.y) < 0.05f) 
+            rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+    }
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            Jump = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D collision)
+    {
+        Jump = false;
+    }
+}
